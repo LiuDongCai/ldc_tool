a957f4ee6a1a7204cb5c5a7e9ae4f5caa1a477b6
未央歌 <liudongcai@addcn.com>
2024-03-29 11:19:34 +0800

    create

---
+++ android/app/src/main/kotlin/com/ldc/ldc_tool/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ldc.ldc_tool
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,125 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:mop/mop.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> init() async {
+    if (Platform.isIOS) {
+      final res = await Mop.instance.initialize(
+          '22LyZEib0gLTQdU3MUauATBwgfnTCJjdr7FCnywmAEM=', 'bdfd76cae24d4313',
+          apiServer: 'https://api.finclip.com', apiPrefix: '/api/v1/mop');
+      print(res);
+    } else if (Platform.isAndroid) {
+      final res = await Mop.instance.initialize(
+          '22LyZEib0gLTQdU3MUauATBwgfnTCJjdr7FCnywmAEM=', 'bdfd76cae24d4313',
+          apiServer: 'https://api.finclip.com', apiPrefix: '/api/v1/mop');
+      print(res);
+    }
+    if (!mounted) return;
+  }
+
+  // 5e637a18cbfae4000170fa7a
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('凡泰极客小程序 Flutter 插件'),
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.only(
+              top: 20,
+            ),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  width: 140,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    gradient: LinearGradient(
+                      colors: const [Color(0xFF12767e), Color(0xFF0dabb8)],
+                      stops: const [0.0, 1.0],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      Mop.instance.openApplet('5facb3a52dcbff00017469bd',
+                          path: 'pages/index/index', query: '');
+                    },
+                    child: Text(
+                      '打开画图小程序',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                Container(
+                  width: 140,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    gradient: LinearGradient(
+                      colors: const [Color(0xFF12767e), Color(0xFF0dabb8)],
+                      stops: const [0.0, 1.0],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      Mop.instance.openApplet('5fa214a29a6a7900019b5cc1');
+                    },
+                    child: Text(
+                      '打开官方小程序',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                Container(
+                  width: 140,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    gradient: LinearGradient(
+                      colors: const [Color(0xFF12767e), Color(0xFF0dabb8)],
+                      stops: const [0.0, 1.0],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      Mop.instance.openApplet('5fa215459a6a7900019b5cc3');
+                    },
+                    child: Text(
+                      '我的对账单',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
